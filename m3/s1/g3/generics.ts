b6b4cccd32eaaca92af3466c17f6d834af9e9f97
captain-mike <info@grafikweb.it>
2023-07-19 13:35:21 +0200

    commenti

--- m3/s1/g3/generics.ts
+++ m3/s1/g3/generics.ts
@@ -6,11 +6,19 @@
 // }
 
 
+/*
+La lettera T che vedi tra le parentesi uncinate indica che la funzione andrà ad utilizzare un generic chiamato T(ma poteva essere chiamato anche diversamente)
+Questo permnetterà di "parametrizzare" anche i tipi di dato, quindi successivamente andremo a dire che T corrisponde ad un tipo di dato specifico
+*/
 function sommaConcatena<T>(a:T, b:T):T{
     return (a as any) + (b as any);
 }
 
-
+/**
+ * Siccome sommaConcatena() utilizza un generic(quello chiamato T alla riga 13)
+ * l'indicazione <string> o <number> va ad indicare quale sarà il tipo di dato che viene utilizzato per sopstituire il generic
+ * In parole povere se scrivo <string> è come se la funzione cambiasse così => sommaConcatena(a:string, b:string):string
+ */
 console.log( sommaConcatena<string>('ciao',' ciao') );
 console.log( sommaConcatena<number>(5,5) );
 
