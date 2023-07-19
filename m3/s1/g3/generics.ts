96d7b9ecf6019fd32f2f62bb714e1c859682d1da
captain-mike <info@grafikweb.it>
2023-07-19 13:38:35 +0200

    commenti

--- m3/s1/g3/generics.ts
+++ m3/s1/g3/generics.ts
@@ -22,15 +22,18 @@ function sommaConcatena<T>(a:T, b:T):T{
 console.log( sommaConcatena<string>('ciao',' ciao') );
 console.log( sommaConcatena<number>(5,5) );
 
-
+//qui invece abbiamo 2 generics
 interface KeyPair<T,U>{
-    key:T,
-    value:U;
+    key:T,//T va a tipizzare la proprietà key
+    value:U;//U tipizza value
 }
 
-let a:KeyPair<string, number> = {
+//in fase di utilizzo di KeyPair devo quindi ricordarmi di definire i tipi di dato corrispondenti ai generic, come qui sotto
+let a:KeyPair<string, number> = {//quello che a riga 26 è T ora è string, mentre U è diventato number
     key: "",
     value: 0
 }
+
+// i generic ci permettono dunque di rendewre variabili i tipi di dato, per estendere il numero di possibili utilizzi di una funzione/classe/interfaccia
 
 
