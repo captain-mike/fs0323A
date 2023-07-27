5cc04f8858ee58ab95a21005e64d77b5716c051a
captain-mike <info@grafikweb.it>
2023-07-27 14:21:58 +0200

    commenti

--- m3/s2/g4/pipe-directive/src/app/cut.pipe.ts
+++ m3/s2/g4/pipe-directive/src/app/cut.pipe.ts
@@ -5,7 +5,9 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class CutPipe implements PipeTransform {
 
+  //questa pipe taglia il testo riducendone la lunghezza a 10 caratteri, aggiungendo i 3 puntini nel punto in cui il testo viene troncato
   transform(value: string, ...args: unknown[]): string {
+    //value è il testo sottoposto alla pipe
     return value.substring(0,10) + '...';
   }
 
